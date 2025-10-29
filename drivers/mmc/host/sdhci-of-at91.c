sartura/linux

mmc: mmc: sdhci-of-at91: add LAN969x
Add support for LAN969x SoC.

Signed-off-by: Robert Marko <robert.marko@sartura.hr>

drivers/mmc/host/sdhci-of-at91.c
@@ -235,10 +235,18 @@ static const struct sdhci_at91_soc_data soc_data_sama7g5 = {
 	.quirks2 = SDHCI_QUIRK2_AT91_HS400_PRESET,
 };
 
+static const struct sdhci_at91_soc_data soc_data_lan969x = {
+	.pdata = &sdhci_sama5d2_pdata,
+	.max_sdr104_clk = 100000000,
+	.baseclk_is_generated_internally = true,
+	.divider_for_baseclk = 2,
+};
+
 static const struct of_device_id sdhci_at91_dt_match[] = {
 	{ .compatible = "atmel,sama5d2-sdhci", .data = &soc_data_sama5d2 },
 	{ .compatible = "microchip,sam9x60-sdhci", .data = &soc_data_sam9x60 },
 	{ .compatible = "microchip,sama7g5-sdhci", .data = &soc_data_sama7g5 },
+	{ .compatible = "microchip,lan969x-sdhci", .data = &soc_data_lan969x },
 	{}
 };
 MODULE_DEVICE_TABLE(of, sdhci_at91_dt_match);

henrymao-zz/novarq-linux

drivers/mmc/host/sdhci-of-at91.c
@@ -235,10 +235,18 @@ static const struct sdhci_at91_soc_data soc_data_sama7g5 = {
 	.quirks2 = SDHCI_QUIRK2_AT91_HS400_PRESET,
 };
 
+static const struct sdhci_at91_soc_data soc_data_lan969x = {
+	.pdata = &sdhci_sama5d2_pdata,
+	.max_sdr104_clk = 100000000,
+	.baseclk_is_generated_internally = true,
+	.divider_for_baseclk = 2,
+};
+
 static const struct of_device_id sdhci_at91_dt_match[] = {
 	{ .compatible = "atmel,sama5d2-sdhci", .data = &soc_data_sama5d2 },
 	{ .compatible = "microchip,sam9x60-sdhci", .data = &soc_data_sam9x60 },
 	{ .compatible = "microchip,sama7g5-sdhci", .data = &soc_data_sama7g5 },
+	{ .compatible = "microchip,lan9691-sdhci", .data = &soc_data_lan969x },
 	{}
 };
 MODULE_DEVICE_TABLE(of, sdhci_at91_dt_match);